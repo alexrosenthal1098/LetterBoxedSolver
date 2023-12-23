2d94ea1c2bf1bc6de98bceb28f654a9959f1c2a0
Alex Rosenthal <alexrosen445@gmail.com>
2023-12-23 18:52:55 -0500

    created plist file

--- auto_commit.sh
+++ auto_commit.sh
@@ -2,6 +2,8 @@
 
 cd /Users/alexrosenthal/LetterBoxedSolver
 
+python3 src/collect_lb_words.py
+
 git add docs/lb_words.txt
-git commit -m "auto commit"
+git commit -m "auto commit new words"
 git push origin main
